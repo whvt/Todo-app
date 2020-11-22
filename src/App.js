38b97b4869a53731ef7c0ce43c29c38b136e97b8
Nikita <xm987654321@gmail.com>
2020-11-23 02:41:51 +0300

    complete app

--- src/App.js
+++ src/App.js
@@ -1,50 +1,77 @@
-import React from "react"
-import TodoList from "./ToDo/TodoList"
-import Context from './context'
-import AddTodo from './ToDo/AddTodo'
+import React, { useEffect } from "react";
+import TodoList from "./ToDo/TodoList";
+import Context from "./context";
+import Loader from "./Loader";
+import Modal from "./Modal/Modal";
 
+const AddTodo = React.lazy(
+  () =>
+    new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(import("./ToDo/AddTodo"));
+      }, 2000);
+    })
+);
 
 function App() {
+  const [todos, setTodos] = React.useState([]);
+  const [loading, setloading] = React.useState(true);
 
-  const [todos, setTodos] = React.useState([
-    {id:1, completed:false,title:'Купить хлеб'},
-    {id:2, completed:false,title:'Купить сахар'},
-    {id:3, completed:false,title:'Купить соль'}
-  ]) 
-  
-  function toggleTodo (id) {
-    setTodos(todos.map(todo => {
-      if (todo.id === id) {
-        todo.completed = !todo.completed
-      }
-      return todo
-    }))    
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos?_limit=5")
+      .then((response) => response.json())
+      .then((todos) => {
+        setTimeout(() => {
+          setTodos(todos);
+          setloading(false);
+        }, 2000);
+      });
+  }, []);
+
+  function toggleTodo(id) {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    );
   }
 
-  function removeTodo (id) {
-    setTodos(todos.filter(todo => todo.id !== id))
+  function removeTodo(id) {
+    setTodos(todos.filter((todo) => todo.id !== id));
   }
 
-  function addTodo (title) {
-    setTodos(todos.concat([{
-      title,
-      id: Date.now(),
-      completed: false
-    }]))
+  function addTodo(title) {
+    setTodos(
+      todos.concat([
+        {
+          title,
+          id: Date.now(),
+          completed: false,
+        },
+      ])
+    );
   }
 
   return (
     <Context.Provider value={{ removeTodo }}>
       <div className="wrapper">
-      <h1>First react app</h1>      
-      {todos.length ? (<TodoList todos={todos} onToggle={toggleTodo} />
-      ) : (
-      <p>No todos!</p>
-      )}
-      <AddTodo onCreate={addTodo}/>     
+        <h1>Todo app</h1>
+        <Modal />
+        {todos.length ? (
+          <TodoList todos={todos} onToggle={toggleTodo} />
+        ) : loading ? null : (
+          <p>No todos!</p>
+        )}
+        <React.Suspense fallback={<p>Loading...</p>}>
+          <AddTodo onCreate={addTodo} />
+        </React.Suspense>
+        {loading && <Loader />}
       </div>
-    </Context.Provider>    
-  )  
+    </Context.Provider>
+  );
 }
 
 export default App;
--- src/ToDo/AddTodo.js
+++ src/ToDo/AddTodo.js
@@ -1,21 +1,35 @@
 import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 
-function AddTodo({onCreate}) {
-    const [value, setValue] = useState('')
 
+function useInputValue(defaultValue='') {
+    const [value, setValue] = useState(defaultValue)
+
+    return {
+        bind: {
+            value,
+            onChange: event => setValue(event.target.value)
+        },
+        clear: () => setValue(''),
+        value: () => value
+    }
+}
+function AddTodo({onCreate}) {
+
+    const input = useInputValue('')
+    
     function submitHandler(event) {
         event.preventDefault()
 
-        if (value.trim()) {
-            onCreate(value)
-            setValue('')
+        if (input.value().trim()) {
+            onCreate(input.value())
+            input.clear()            
         }
     }
 
     return (
         <form style={{marginBottom:'1rem'}} onSubmit={submitHandler}>
-            <input value={value} onChange={event => setValue(event.target.value)}></input>
+            <input {...input.bind}></input>
             <button type='submit'>Add todo</button>
         </form>
     )
